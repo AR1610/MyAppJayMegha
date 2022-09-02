bd700947e5bfadba8f723824ff5ac32e953d22e4
Aarti Patel <royal.arati10@gmail.com>
2022-09-02 16:36:21 +0530

    Spinner in Android

--- app/src/main/java/com/myappjaymegha/SpinnerActivity.java
+++ app/src/main/java/com/myappjaymegha/SpinnerActivity.java
@@ -1,18 +1,23 @@
 package com.myappjaymegha;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SpinnerActivity extends AppCompatActivity {
 
     Spinner spinner;
-    String strLang[] = {"Android","Java",".net","PHP","C","C++","Python","IOS"}; // Data
+    String strLang[] = {"Select Data","Android","Java",".net","PHP","C","C++","Python","IOS"}; // Data
 
 
     @Override
@@ -22,17 +27,32 @@ public class SpinnerActivity extends AppCompatActivity {
         spinner = findViewById(R.id.spinner);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1,strLang);
+                (this, android.R.layout.simple_list_item_1,strLang){
+            @Override
+            public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                TextView tvData = (TextView) super.getDropDownView(position, convertView, parent);
+                if (position == 0){
+                    tvData.setTextColor(Color.GRAY);
+                }else {
+                    tvData.setTextColor(Color.BLUE);
+
+                }
+                return tvData;
+            }
+        };
         spinner.setAdapter(arrayAdapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-                String strdata = adapterView.getItemAtPosition(i).toString();
-                String strdata1 = strLang[i];
+                if (i !=0 ){
+                    String strdata = adapterView.getItemAtPosition(i).toString();
+                    String strdata1 = strLang[i];
 
-                Toast.makeText(SpinnerActivity.this, ""+strdata1, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SpinnerActivity.this, ""+strdata1, Toast.LENGTH_SHORT).show();
+                }
+
             }
 
             @Override
